be4647edc817c11522f4a38a4dfae30c9702fba1
santiago ottolini <santiago.ottolini98@gmail.com>
2020-02-25 16:00:18 +0100

    SA-BD: Handling cors errors and bodyParser library added

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 const app = express();
 
 const studentsRoutes = require('./api/routes/students')
 
 app.use(morgan('dev'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Acces-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Autorization')
+    if (req.method === 'OPTIONS'){
+        res.header('Acces-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next();
+})
 
 app.use('/students', studentsRoutes)
 
