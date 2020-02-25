ba4c62df2de0fed4756e69c2378fb1e8fd5791f3
santiago ottolini <santiago.ottolini98@gmail.com>
2020-02-25 15:52:08 +0100

    SA-BE: Error handling when route does not exist

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ app.use(morgan('dev'))
 
 app.use('/students', studentsRoutes)
 
+app.use((req,res,next)=>{
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500)
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+})
+
 module.exports = app
