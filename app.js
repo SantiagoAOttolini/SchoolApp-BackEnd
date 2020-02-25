d5b612fcf28c709c9dd48b41558c9c495bc61632
santiago ottolini <santiago.ottolini98@gmail.com>
2020-02-25 14:52:08 +0100

    SA-BE: Server listening

---
+++ app.js
@@ -0,0 +1,11 @@
+/*import Express from "express";*/
+const express = require("express")
+const app = express();
+
+app.use((req, res, next) => {
+  res.status(200).json({
+    message: 'Works'
+  });
+});
+
+module.exports = app
