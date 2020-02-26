2c2401215da76b67b9d2bad264589b46ed6333b3
santiago ottolini <santiago.ottolini98@gmail.com>
2020-02-26 09:41:40 +0100

    SA-BE: Mongoose Atlas DB connection

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express();
 
 const studentsRoutes = require('./api/routes/students')
 
+mongoose.connect(
+    'mongodb+srv://ivansanti:' + process.env.MONGO_ATLAS_PW + '@schoolapp-db-ekfvn.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+)
+
 app.use(morgan('dev'))
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Acces-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Autorization')
-    if (req.method === 'OPTIONS'){
+    if (req.method === 'OPTIONS') {
         res.header('Acces-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
         return res.status(200).json({})
     }
@@ -24,16 +34,16 @@ app.use((req, res, next) => {
 
 app.use('/students', studentsRoutes)
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     const error = new Error('Not found')
     error.status = 404
     next(error)
 })
 
-app.use((error, req, res, next)=>{
+app.use((error, req, res, next) => {
     res.status(error.status || 500)
     res.json({
-        error:{
+        error: {
             message: error.message
         }
     })
