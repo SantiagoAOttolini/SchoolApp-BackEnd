d79eb6e7256d3aef5645c7ab7658182e324ab115
ivanjuko <ivanjukonis@hotmail.com>
2020-02-26 10:32:21 -0300

    SA-BE: Handling POST request from students

--- api/routes/students.js
+++ api/routes/students.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
+
 const Student = require('../models/student') 
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    messsage: 'Test get'
-  })
-})
-
 router.post('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Test post'
+  console.log(req.file)
+  const student = new Student({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      lastname: req.body.lastname,
+      age: req.body.age,
+      class: req.body.class
   })
+
+  student.save()
+      .then(result => {
+          console.log(result)
+          res.status(201).json({
+              message: 'Student created succesfully',
+              createdProduct: {
+                  name: result.name,
+                  lastname: result.lastname,
+                  age: result.age,
+                  class: result.class,
+                  _id: result.id,
+                  request: {
+                      type: 'POST',
+                      url: 'http://localhost:5000/products/'
+                  }
+              }     
+          })
+      })
+      .catch(err => {
+          console.log(err)
+          res.status(500).json({
+              error: err
+          })
+      })
 })
 
 module.exports = router
