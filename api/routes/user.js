56fec6d960afb9a59dc5ed6b11507731b882532f
ivanjuko <ivanjukonis@hotmail.com>
2020-02-27 09:36:37 -0300

    SA-BE: Handling DELETE request from user

--- api/routes/user.js
+++ api/routes/user.js
@@ -88,3 +88,21 @@ router.post("/login", (req, res, next) => {
         });
       });
   });
+
+router.delete("/:userId", (req, res, next) => {
+    User.remove({ _id: req.params.userId })
+      .exec()
+      .then(result => {
+        res.status(200).json({
+          message: "User deleted"
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+    });
+});
+
+module.exports = router
