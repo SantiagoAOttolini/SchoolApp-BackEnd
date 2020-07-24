6cb88cac7f6e5557bce59f6a6a200f8a3dde5bcf
santiago <santiago.ottolini98@gmail.com>
2020-07-23 21:58:15 -0300

    SA-BE: Change build

---
+++ public/build/precache-manifest.fee9caf7b6f224af547c255c95bd728e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77faf241f93fb7f9fd36773bc9c2e97a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03025527541614aac360",
+    "url": "/static/css/main.5041d136.chunk.css"
+  },
+  {
+    "revision": "232f2099a592bb791161",
+    "url": "/static/js/2.c214c2fa.chunk.js"
+  },
+  {
+    "revision": "57f5c77c6cb93e0937628f0205081484",
+    "url": "/static/js/2.c214c2fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03025527541614aac360",
+    "url": "/static/js/main.e6099d20.chunk.js"
+  },
+  {
+    "revision": "f5d4f4fddd23a5cc4f57",
+    "url": "/static/js/runtime-main.3dea5b2d.js"
+  }
+]);
