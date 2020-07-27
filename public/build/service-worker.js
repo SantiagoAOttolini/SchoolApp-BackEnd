f2c8cbf96ecaa12614656a05e853da8ab8fd7943
santiago <santiago.ottolini98@gmail.com>
2020-07-27 15:00:59 -0300

    SA-BE: Change heroku build

--- public/build/service-worker.js
+++ public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7b2f693745972c6fb5e4802625885e1.js"
+  "/precache-manifest.abd36f83f509e3df6584d94cd84d447b.js"
 );
 
 self.addEventListener('message', (event) => {
